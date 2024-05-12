865df663590648240315f12a3f208550cf1dc237
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-12 23:40:30 +0300

    initial commit

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+  email: {type: String, required: true},
+  password: {type: Boolean, required: true},
+  providers: [{type: String, required: true}],
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString()
+
+    delete returnObject.__v
+    delete returnObject._d
+    delete returnObject.password
+  }
+})
+
+
+module.exports = mongoose.model('User', userSchema)
