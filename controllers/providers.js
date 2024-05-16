7b4418784d0d24f0c508a0effee6ba1f54f57a61
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-05-17 01:04:04 +0300

    add integration tests

--- controllers/providers.js
+++ controllers/providers.js
@@ -3,7 +3,7 @@ const Provider = require('../models/provider')
 
 const providerRouter = express.Router()
 
-providerRouter.get('/', async (request,response,next) => {
+providerRouter.get('/', async (request, response, next) => {
   const providers = await Provider.find({})
   try {
     response.send(providers)
@@ -12,13 +12,52 @@ providerRouter.get('/', async (request,response,next) => {
   }
 })
 
-// providerRouter.get('/', async (request,response,next) => {
-//   const providers = await Provider.find({})
-//   try {
-//     response.send(providers)
-//   } catch (error) {
-//     next(error)
-//   }
-// })
+providerRouter.get('/:id', async (request, response, next) => {
+  try {
+    const provider = await Provider.findById(request.params.id)
+    if (!provider) {
+      return response.status(404).end()
+    }
+    response.send(provider)
+  } catch (error) {
+    next(error)
+  }
+})
+
+providerRouter.post('/', async (request, response, next) => {
+  const { name, licenced } = request.body
+  try {
+    const providerObject = new Provider({ name, licenced })
+    const savedProvider = await providerObject.save()
+    response.status(201).send(savedProvider)
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+providerRouter.put('/', async (request, response, next) => {
+  const { name, licenced } = request.body
+  try {
+    const updatedProvider = await Provider.findByIdAndUpdate(
+      request.params.id,
+      { name, licenced },
+      { new: true }
+    )
+    response.send(updatedProvider)
+  } catch (error) {
+    next(error)
+  }
+})
+
+providerRouter.delete('/:id', async (request,response,next) => {
+ 
+  try {
+    await Provider.findByIdAndDelete(request.params.id)
+    response.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = providerRouter
